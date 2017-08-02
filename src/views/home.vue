b78e6f8cfc35d32d9938e0562eba9056436a9632
leon <fe.lizhoukai@gmail.com>
2017-08-02 15:27:44 +0800

    修改

---
+++ src/views/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    首页
+  </div>
+</template>
+<script>
+import util from 'util'
+
+export default {
+  name: '',
+  data() {
+    return {
+    }
+  },
+  props: {},
+  computed: {},
+  methods: {},
+  mounted() {
+    util.get('/api/v1/topics/', {}, res => {})
+  }
+}
+</script>
+<style lang='less' scoped>
+</style>
